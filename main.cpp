249e6bd91db58d6029464e0068345e95f88f03d6
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2021-02-27 16:53:57 -0500

    Implement AI with negamax and pruning

--- main.cpp
+++ main.cpp
@@ -60,6 +60,15 @@ class ur_board {
             memset(black_pieces, 0, sizeof(black_pieces));
         }
 
+        ur_board(bool* white, int wrem, bool* black, int brem) {
+            for(int i = 0; i < 14; ++i) {
+                white_pieces[i] = white[i];
+                black_pieces[i] = black[i];
+            }
+            white_rem = wrem;
+            black_rem = brem;
+        }
+
         bool* get_pieces(bool white_turn) {
             if(white_turn) {
                 return white_pieces;
@@ -369,15 +378,15 @@ class ur_game {
         void no_moves(int roll) {
             cout << (white_turn ? "WHITE" : "BLACK") << " had no valid moves for roll " << roll << endl;
         }
-        
+
         void move() {
-            board.display_board();
+            //board.display_board();
             int roll = 0;
             for(int i = 0; i < 4; ++i) {
                 roll += rand() % 2;
             }
             if(!board.has_valid(roll, white_turn)) {
-                no_moves(roll);
+                //no_moves(roll);
                 board.no_moves(white_turn);
                 white_turn = !white_turn;
                 return;
@@ -386,7 +395,7 @@ class ur_game {
             while(!board.is_valid(roll, tile, white_turn)) {
                 tile = get_move(roll);
             }
-            display_move(roll, tile);
+            //display_move(roll, tile);
             board.move_piece(tile, tile + roll, white_turn);
             if(!board.is_rosette(tile + roll)) {
                 white_turn = !white_turn;
@@ -401,16 +410,187 @@ class ur_game {
         }
 };
 
+class ai_player : public virtual ur_player {
+    private:
+        bool is_white;
+    public:
+        ai_player(bool white) {
+            is_white = white;
+        }
+
+        double value_of(ur_board& b, bool white_turn) {
+            bool* pieces = b.get_pieces(white_turn);
+            int rem = b.get_rem(white_turn);
+            bool* opp_pieces = b.get_pieces(!white_turn);
+            int opp_rem = b.get_rem(!white_turn);
+            int board = 0;
+            int opp_board = 0;
+            for(int i = 0; i < 14; ++i) {
+                if(pieces[i]) {
+                    ++board;
+                }
+                if(opp_pieces[i]) {
+                    ++opp_board;
+                }
+            }
+            if(board == 0 && rem == 0) {
+                return 1000.0;
+            }
+            if(opp_board == 0 && opp_rem == 0) {
+                return -1000.0;
+            }
+            int done = 7 - board - rem;
+            int opp_done = 7 - opp_board - opp_rem;
+            return 10.0 * done + 4.0 * board - 10.0 * opp_done - 4.0 * opp_board;
+        }
+
+        double get_avg(ur_board& board, int depth, bool white_turn, double alpha, double beta) {
+            double rates[] = {0.0625, 0.25, 0.375, 0.25, 0.0625};
+            double ans = 0;
+            for(int i = 0; i <= 4; ++i) {
+                ans += rates[i] * negamax(board, i, white_turn, depth, alpha, beta).second;
+            }
+            return ans;
+        }
+
+        int find_any(bool* pieces, int rem) {
+            if(rem > 0) {
+                return -1;
+            }
+            for(int i = 0; i < 14; ++i) {
+                if(pieces[i]) {
+                    return i;
+                }
+            }
+            throw -1;
+        }
+
+        pair<int, double> negamax(ur_board& board, int roll, bool white_turn, int depth, double alpha, double beta) {
+            if(board.winner() || depth > 4) {
+                return make_pair(-2, value_of(board, white_turn));
+            }
+            if(roll == 0) {
+                int any_free = find_any(board.get_pieces(white_turn), board.get_rem(white_turn));
+                board.move_piece(any_free, any_free + roll, white_turn);
+                pair<int, double> ans = make_pair(any_free, -get_avg(board, depth + 1, !white_turn, -beta, -alpha));
+                board.undo_last();
+                return ans;
+            }
+            if(!board.has_valid(roll, white_turn)) {
+                board.no_moves(white_turn);
+                pair<int, double> ans = make_pair(-2, -get_avg(board, depth + 1, !white_turn, -beta, -alpha));
+                board.undo_last();
+                return ans;
+            }
+            vector<pair<double, int>> moves;
+            for(int i = -1; i < 14; ++i) {
+                if(board.is_valid(roll, i, white_turn)) {
+                    board.move_piece(i, i + roll, white_turn);
+                    if(!board.is_rosette(i + roll)) {
+                        moves.push_back(make_pair(-value_of(board, !white_turn), i));
+                    } else {
+                        moves.push_back(make_pair(value_of(board, white_turn), i));
+                    }
+                    board.undo_last();
+                }
+            }
+            sort(moves.begin(), moves.end());
+            double cmax = -numeric_limits<double>::max();
+            int mmax = -2;
+            for(int i = (int) moves.size() - 1; i >= 0; --i) {
+                int tile = moves[i].second;
+                if(!board.is_valid(roll, tile, white_turn)) {
+                    throw tile;
+                }
+                board.move_piece(tile, tile + roll, white_turn);
+                double ans;
+                if(!board.is_rosette(tile + roll)) {
+                    ans = -get_avg(board, depth + 1, !white_turn, -beta, -alpha);
+                } else {
+                    ans = get_avg(board, depth + 1, white_turn, alpha, beta);
+                }
+                board.undo_last();
+                if(ans >= cmax) {
+                    cmax = ans;
+                    mmax = tile;
+                }
+                alpha = max(alpha, cmax);
+                if(beta <= alpha) {
+                    break;
+                }
+            }
+            return make_pair(mmax, cmax);
+        }
+
+        int get_move(bool* pieces, int rem, bool* opp_pieces, int opp_rem, int roll) {
+            if(roll == 0) {
+                return find_any(pieces, rem);
+            }
+            if(rem > 0) {
+                bool any = false;
+                for(int i = 0; i < 14; ++i) {
+                    if(pieces[i]) {
+                        any = true;
+                        break;
+                    }
+                }
+                if(!any) {
+                    return -1;
+                }
+            }
+            ur_board board;
+            if(is_white) {
+                board = ur_board(pieces, rem, opp_pieces, opp_rem);
+            } else {
+                board = ur_board(opp_pieces, opp_rem, pieces, rem);
+            }
+            double inf = numeric_limits<double>::max();
+            double neginf = -inf;
+            pair<int, double> ans = negamax(board, roll, is_white, 0, neginf, inf);
+            return ans.first;
+        }
+};
+
 int main(int argc, char* argv[]) {
     int seed = time(NULL);
     if(argc > 1) {
         seed = atoi(argv[1]);
     }
+    cout << "Seed: " << seed << endl;
+    /*
+    // test one
     srand(seed);
     random_player h(true);
-    random_player r(false);
+    ai_player r(false);
     ur_game game(h, r);
     cout << (game.start() == 1 ? "WHITE WON" : "BLACK WON") << endl;
+    */
+    // test multiple
+    int total = 0;
+    int beat = 0;
+    for(int i = 0; i < 50; ++i) {
+        int color = rand() % 2;
+        srand(i ^ seed);
+        if(color == 0) {
+            ai_player a(true);
+            random_player b(false);
+            ur_game game(a, b);
+            if(game.start() == 1) {
+                ++beat;
+            }
+            ++total;
+        } else {
+            ai_player a(false);
+            random_player b(true);
+            ur_game game(b, a);
+            if(game.start() == 2) {
+                ++beat;
+            }
+            ++total;
+        }
+        cout << "Finished match " << i + 1 << endl;
+    }
+    cout << (double) beat / total << endl;
     cout << "Program end." << endl;
     return 0;
 }
